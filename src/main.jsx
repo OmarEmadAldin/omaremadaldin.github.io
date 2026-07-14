fc0c66e7aaf73db7970cc5cb241f3f4e357b9da2
OmarEmadAldin <112203101+OmarEmadAldin@users.noreply.github.com>
2026-07-14 18:45:12 +0300

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -3,24 +3,6 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 
-document.documentElement.classList.add('no-transition')
-
-// Apply saved theme
-if (
-  localStorage.theme === 'dark' ||
-  (!localStorage.theme &&
-    window.matchMedia('(prefers-color-scheme: dark)').matches)
-) {
-  document.documentElement.classList.add('dark')
-} else {
-  document.documentElement.classList.remove('dark')
-}
-
-// Re-enable transitions
-setTimeout(() => {
-  document.documentElement.classList.remove('no-transition')
-}, 50)
-
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
